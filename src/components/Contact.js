dde1eb7ea0cce907a316d211a8e7efc0d45f63ff
mehmetguduk <mehmetguduk98@gmail.com>
2022-05-19 17:21:36 +0300

    master

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,87 +1,46 @@
 import React from "react"
 
 export default function Contact() {
-
-    const [formData, setFormData] = React.useState({
-        name: "",
-        email: "",
-        subject: "",
-        message: ""
-    })
-
-    function formChangeHandler(event) {
-        const { name, value } = event.target;
-        setFormData((prevFormData) => {
-            return ({
-                ...prevFormData,
-                [name]: [value]
-            })
+    /*
+        const [formData, setFormData] = React.useState({
+            name: "",
+            email: "",
+            subject: "",
+            message: ""
         })
-    }
-
-
+    
+        function formChangeHandler(event) {
+            const { name, value } = event.target;
+            setFormData((prevFormData) => {
+                return ({
+                    ...prevFormData,
+                    [name]: [value]
+                })
+            })
+        }
+    
+    */
 
     return (
         <section id="contact">
             <h2 class="section-title">CONTAC<span>T</span></h2>
-            <form
-                class="contact-form"
-                data-netlify="true"
-                action="/"
-                name="contact"
-            >
-                <p class="form-error"></p>
-                <input
-                    id="contact-name"
-                    name="name"
-                    value={formData.name}
-                    onChange={formChangeHandler}
-                    type="text"
-                    placeholder="Name"
-                    autoComplete="off"
-                    spellCheck="false"
-                    required
-                    minLength="2"
-                />
-                <input
-                    id="contact-email"
-                    name="email"
-                    value={formData.email}
-                    onChange={formChangeHandler}
-                    type="email"
-                    placeholder="Email"
-                    autoComplete="off"
-                    spellCheck="false"
-                    required
-                />
-                <input
-                    id="contact-subject"
-                    name="subject"
-                    value={formData.subject}
-                    onChange={formChangeHandler}
-                    type="text"
-                    placeholder="Subject"
-                    autoComplete="off"
-                    spellCheck="false"
-                    required
-                    minLength="2"
-                />
-                <textarea
-                    id="contact-message"
-                    name="message"
-                    value={formData.message}
-                    onChange={formChangeHandler}
-                    placeholder="Message"
-                    autoComplete="off"
-                    spellCheck="false"
-                    required
-                    minLength="20"
-                />
-                <button
-                    class="form-submit"
-                    type="submit">
-                    Submit <i class="fa fa-paper-plane" aria-hidden="true"></i>
-                </button>
+            <form name="contact" method="post">
+                <input type="hidden" name="form-name" value="contact" />
+                <p>
+                    <label htmlFor="name">Name</label> <br />
+                    <input type="text" id="name" name="name" required />
+                </p>
+                <p>
+                    <label htmlFor="email">Email</label> <br />
+                    <input type="email" id="email" name="email" required />
+                </p>
+                <p>
+                    <label htmlFor="message">Message</label> <br />
+                    <textarea id="message" name="message" required></textarea>
+                </p>
+                <p>
+                    <input type="submit" value="Submit message" />
+                </p>
             </form>
         </section>
     )
