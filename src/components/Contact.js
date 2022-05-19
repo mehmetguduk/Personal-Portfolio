c75c9987ffe9d7684a819d8cc0f35902c51650bb
mehmetguduk <mehmetguduk98@gmail.com>
2022-05-19 14:20:36 +0300

    master

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,96 @@
+import React from "react"
+
 export default function Contact() {
+
+    const [formData, setFormData] = React.useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+    })
+
+    function formChangeHandler(event) {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => {
+            return ({
+                ...prevFormData,
+                [name]: [value]
+            })
+        })
+    }
+
+    function formSubmitHandler(event) {
+        event.preventDefault()
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+            .then(() => console.log("Form successfully submitted"))
+            .catch((error) => alert(error));
+    }
+
     return (
         <section id="contact">
             <h2 class="section-title">CONTAC<span>T</span></h2>
-            <form class="contact-form" data-netlify="true" name="contact">
+            <form
+                class="contact-form"
+                data-netlify="true"
+                name="contact"
+                onSubmit={formSubmitHandler}
+            >
                 <p class="form-error"></p>
-                <input id="contact-name" name="name" type="text" placeholder="Name" autocomplete="off" spellcheck="false" />
-                <input id="contact-email" name="email" type="text" placeholder="Email" autocomplete="off"
-                    spellcheck="false" />
-                <input id="contact-subject" name="subject" type="text" placeholder="Subject" autocomplete="off"
-                    spellcheck="false" />
-                <textarea id="contact-message" name="message" placeholder="Message" autocomplete="off"
-                    spellcheck="false"></textarea>
-                <button class="form-submit" type="button">Submit <i class="fa fa-paper-plane"
-                    aria-hidden="true"></i></button>
+                <input
+                    id="contact-name"
+                    name="name"
+                    value={formData.name}
+                    onChange={formChangeHandler}
+                    type="text"
+                    placeholder="Name"
+                    autoComplete="off"
+                    spellCheck="false"
+                    required
+                    minLength="2"
+                />
+                <input
+                    id="contact-email"
+                    name="email"
+                    value={formData.email}
+                    onChange={formChangeHandler}
+                    type="email"
+                    placeholder="Email"
+                    autoComplete="off"
+                    spellCheck="false"
+                    required
+                />
+                <input
+                    id="contact-subject"
+                    name="subject"
+                    value={formData.subject}
+                    onChange={formChangeHandler}
+                    type="text"
+                    placeholder="Subject"
+                    autoComplete="off"
+                    spellCheck="false"
+                    required
+                    minLength="2"
+                />
+                <textarea
+                    id="contact-message"
+                    name="message"
+                    value={formData.message}
+                    onChange={formChangeHandler}
+                    placeholder="Message"
+                    autoComplete="off"
+                    spellCheck="false"
+                    required
+                    minLength="20"
+                />
+                <button
+                    class="form-submit"
+                    type="submit">
+                    Submit <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                </button>
             </form>
         </section>
     )
