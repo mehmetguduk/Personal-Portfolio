c1bae782de5d8778d2ee04ce26a0432132da2c2b
mehmetguduk <mehmetguduk98@gmail.com>
2022-06-21 01:03:42 +0300

    master

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './styles/aboutme.css';
-import './styles/contact.css';
-import './styles/footer.css';
-import './styles/navbar.css';
-import './styles/projects.css';
-import './styles/style.css';
-import './styles/welcome.css';
+import './styles/aboutme.scss';
+import './styles/contact.scss';
+import './styles/footer.scss';
+import './styles/navbar.scss';
+import './styles/projects.scss';
+import './styles/style.scss';
+import './styles/welcome.scss';
 
 
 
