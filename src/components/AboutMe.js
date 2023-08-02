1fb9f0a09cdd09324ac5585a8fc07b3c0ee92d3f
mehmetguduk <mehmetguduk98@gmail.com>
2023-08-02 21:14:22 +0300

    master

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,18 +4,17 @@ export default function AboutMe() {
             <div class="article-container">
                 <h2 class="section-title">ABOUT M<span>E</span></h2>
                 <p class="aboutme-text">
-                    Hello There! I'm Mehmet Güdük. I was born on November 05, 1998 in Ankara, Turkey. As someone who grew up
-                    with computer technology, any work in relation to computers is a massive passion for me. I discovered my
-                    interest in design when I installed Adobe Photoshop on my computer a long time ago. Working on
-                    photos for hours with some chill background music was utterly joyful. Later, I continued this interest
-                    by making vector drawings with Adobe Photoshop Illustrator. Then realized that I can turn this into web
-                    development.
+                    Hello there! I'm Mehmet Güdük, born on November 05, 1998, in Ankara, Turkey. From an early age, I've been immersed in the world of computer technology, and my passion for anything related to computers has only grown stronger with time. My journey into design began when I first installed Adobe Photoshop on my computer many years ago. Spending hours engrossed in editing photos with some soothing background music brought me immense joy.
                     <br /><br />
-                    At the beginning I learned the basics of Javascript, HTML5, CSS through courses that I found on the
-                    internet. Later, with the clones and demos I made, I developed my web design knowledge by constantly
-                    encountering errors and solving them. I continue to improve myself every day.
+                    Over time, my interest in design evolved, and I ventured into creating vector drawings using Adobe Illustrator. This artistic pursuit gradually led me into the world of web development. I embarked on my learning journey by diving into courses on Javascript, HTML5, and CSS, all of which I discovered on the vast expanse of the internet.
                     <br /><br />
-                    During my encounter of several projects, I have mastered the following tools: HTML5 markup language; CSS style sheet language and SASS-SCSS variations; Javascript, TypeScript, Python programming languages; React, Node.JS, Angular, Bootstrap frameworks and libraries; jQuery and plugins made with jQuery; MongoDB, MySQL, SQLite database systems; AWS hosting service; Kali Linux, Windows 10 operating systems; Notepad++, PyCharm, Visual Studio Code compilers; Git, GitHub version controllers.
+                    As I delved deeper into web development, I honed my skills by crafting clones and demos, relentlessly encountering challenges and solving them with tenacity. Each project became a stepping stone towards my growth, and I make it a point to continually push to improve myself  every single day.
+                    <br /><br />
+                    Throughout my diverse projects, I've mastered an array of tools and technologies, including the HTML5 markup language, CSS styles, and various SASS-SCSS variations. I'm proficient in programming languages such as Javascript, TypeScript, and Python. Additionally, I've worked extensively with frameworks and libraries such as React, Node.JS, Angular, and Bootstrap, all while expertly handling jQuery and its plugins.
+                    <br /><br />
+                    When it comes to databases, I have some experience working with MongoDB, MySQL, and SQLite. While I've explored AWS hosting services for deploying applications, my expertise in these areas is still a work in progress. I'm continually learning and refining my skills to become more proficient in utilizing these technologies effectively. My proficiency extends to operating systems like Kali Linux and Windows 10. For development, I rely on Notepad++, PyCharm, and Visual Studio Code compilers. And of course, I actively use Git and GitHub as version control tools to streamline collaboration.
+                    <br /><br />
+                    I'm passionate about creating meaningful and user-friendly digital experiences, and I'm excited to continue my journey of growth and discovery in the world of web development. Let's build something incredible together!
                 </p>
             </div>
         </article>
